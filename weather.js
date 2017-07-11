752b3e58899f65e13a1904739353a2ee4ebfe8ce
Ahmet Akgul <akgualhmet@Outlook.com>
2017-07-11 00:27:13 -0400

    Do front end improvements to feel of app.  Add feature to show forecast for both celcius and fahrenheit.

--- weather.js
+++ weather.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
   var x = document.getElementById("demo");
-  var temp_f, temp_c;
+  var temp_f, temp_c, desc_metric, desc_usa;
 
   $(".tempunits").click(function(){
     if($(this).text()=="\u{00B0}"+"C"){
       $(this).text("\u{00B0}"+"F");
       console.log($(this).text());
-      $("#temperature").html(temp_f);
+      $("#temperature").html(temp_f + "\u{00B0}");
+      $("#condition").html(desc_usa);
     } else if($(this).text()=="\u{00B0}"+"F"){
       $(this).text("\u{00B0}"+"C");
       console.log($(this).text());
-      $("#temperature").html(temp_c);
+      $("#temperature").html(temp_c + "\u{00B0}");
+      $("#condition").html(desc_metric);
     }
   });
 
@@ -18,7 +20,7 @@ $(document).ready(function(){
     navigator.geolocation.getCurrentPosition(function setPosition(position){
       $.ajax( {
 
-        url:"https://api.wunderground.com/api/1699727eaedf2f6e/conditions/q/"
+        url:"https://api.wunderground.com/api/1699727eaedf2f6e/forecast/conditions/q/"
           + position.coords.latitude + "," + position.coords.longitude + ".json",
         dataType: "json",
 
@@ -31,15 +33,17 @@ $(document).ready(function(){
 
           temp_c = data.current_observation.temp_c;
           temp_f = data.current_observation.temp_f;
+          desc_metric = data.forecast.txt_forecast.forecastday[0].fcttext_metric;
+          desc_usa = data.forecast.txt_forecast.forecastday[0].fcttext;
 
           console.log(this.url);
           $("#location").html(data.current_observation.observation_location.full);
           console.log(data.current_observation.observation_location.full);
-          $("#description").html(data.current_observation.weather);
+          // $("#description").html(data.current_observation.weather);
           console.log(data.current_observation.weather);
-          $("#condition").html("Condition");
-          console.log(data.current_observation.temp_f);
-          $("#temperature").html(data.current_observation.temp_c);
+          $("#condition").html(data.forecast.txt_forecast.forecastday[0].fcttext_metric);
+          console.log(data.forecast.txt_forecast.forecastday[0].fcttext_metric);
+          $("#temperature").html(data.current_observation.temp_c + "\u{00B0}");
           console.log(data.current_observation.temp_c);
 
           $("#icon").attr("src", data.current_observation.icon_url);
